2f4eed749c665521f3e92c50e3215cba8819e4f2
Canwdev <c2097@live.com>
2023-04-09 13:49:35 +0800

    dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '', // 👈
+  base: '',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [vue()],
   server: {
     port: 8080
