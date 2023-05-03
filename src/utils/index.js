0ea9173bb555d543d0d821fa40154fcf8a10108c
canwdev <c2097@live.com>
2023-05-03 19:18:39 +0800

    add: auto hide ui

--- src/utils/index.js
+++ src/utils/index.js
@@ -12,4 +12,51 @@ export function snapVideoImageDownload(video) {
   link.download = 'video-screenshot.png';
   link.href = dataUrl;
   link.click();
-};
+}
+
+export class CursorHider {
+  constructor(cursorSelector, showHideFn, time = 1000) {
+    this.timeoutID = null;
+    this.targetEl = document.querySelector(cursorSelector);
+    this.showHideFn = showHideFn;
+    this.time = time
+    this.showCursor = this.showCursor.bind(this);
+
+    this.start()
+  }
+
+  hideCursor() {
+    if (typeof this.showHideFn === 'function') {
+      this.showHideFn({el: this.targetEl, isShow: false})
+    } else {
+      this.targetEl.style.cursor = 'none';
+    }
+  }
+
+  showCursor() {
+    if (typeof this.showHideFn === 'function') {
+      this.showHideFn({el: this.targetEl, isShow: true})
+    } else {
+      this.targetEl.style.cursor = '';
+    }
+    this.runTimer()
+  }
+
+  runTimer() {
+    clearTimeout(this.timeoutID);
+    this.timeoutID = setTimeout(() => {
+      this.hideCursor();
+    }, this.time);
+  }
+
+  start() {
+    document.addEventListener('mousemove', this.showCursor);
+    this.runTimer()
+
+  }
+
+  stop() {
+    document.removeEventListener('mousemove', this.showCursor);
+    clearTimeout(this.timeoutID);
+  }
+}
