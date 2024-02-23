50fff3a81a045079c46da95555022d19e7e93089
canwdev <9585126+canwdev@users.noreply.github.com>
2024-02-23 21:29:00 +0800

    build

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
-  build: {
-    outDir: 'docs'
-  },
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
